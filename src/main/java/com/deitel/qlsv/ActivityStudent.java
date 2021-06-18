f588fc132138b30208f96300fe651549b7352405
= <=>
2021-06-18 21:18:03 +0700

    add create update delete subject

---
+++ src/main/java/com/deitel/qlsv/ActivityStudent.java
@@ -0,0 +1,14 @@
+package com.deitel.qlsv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+    }
+}
